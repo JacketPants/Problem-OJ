d966929a9d41f9dd7476f9b6fe9a8c74698b5389
Joe <joezhang@outlook.com>
2018-02-23 16:27:39 +0800

    AC

--- source/poj-2485.cpp
+++ source/poj-2485.cpp
@@ -10,38 +10,25 @@ int g[MAXN][MAXN]; //邻接矩阵
 int n, m;          //点和边
 int Prim()
 {
+    bool arr[MAXN] = {false};
     int ans = 0, mark = 0;
     int dis[MAXN] = {0};
     for (int i = 0; i < n; i++)
         dis[i] = INF;
-    bool isArr[MAXN] = {false};
-    isArr[0] = true;
     dis[0] = 0;
     for (int i = 0; i < n; i++)
-        if (g[0][i] && g[0][i] < dis[i])
-            dis[i] = g[0][i];
-    //测试代码
-
-    // for (int k = 0; k < n; k++)
-    //     cout << dis[k] << '\t';
-    // cout << endl;
-    for (int i = 1; i < n; i++)
     {
-        mark = 0;
+        mark = -1;
         for (int j = 0; j < n; j++)
-            if (dis[j] && (dis[j] < dis[mark] || !mark))
+            if (!arr[j] && (dis[j] < dis[mark] || mark == -1))
                 mark = j;
-        isArr[mark] = true;
-        ans = max(ans, dis[mark]);
+        if(mark==-1)//不成立的判定条件
+            break;
+        arr[mark] = true;
+        ans = ans < dis[mark] ? dis[mark] : ans;
         for (int j = 0; j < n; j++)
             if (g[mark][j] && g[mark][j] < dis[j])
                 dis[j] = g[mark][j];
-        dis[mark] = 0;
-        //测试代码
-
-        // for (int k = 0; k < n; k++)
-        //     cout << dis[k] << '\t';
-        // cout << endl;
     }
     return ans;
 }
@@ -51,7 +38,6 @@ int main()
     scanf("%d", &t);
     while (t--)
     {
-        memset(g, 0, sizeof(g));
         scanf("%d", &n);
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
@@ -60,17 +46,3 @@ int main()
     }
     return 0;
 }
-/*
-6 10
-1 2 6
-1 3 1
-1 4 5
-2 5 3
-2 3 5
-5 3 6
-5 6 6
-3 6 4
-3 4 5
-6 4 2
-*/
-/*答案是15*/
