6105f6bd5fa6238547645e96d67d07833757fcd9
Joe <joezhang@outlook.com>
2018-03-06 19:34:19 +0800

    AC

---
+++ source/poj-1469.cpp
@@ -0,0 +1,63 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#define MAXN 1000
+using namespace std;
+vector<int> g[MAXN];
+int from[MAXN], ans, n;
+bool use[MAXN];
+
+bool Match(int x)
+{
+    for (int i = 0; i < g[x].size(); i++)
+        if (!use[g[x][i]])
+        {
+            use[g[x][i]] = true;
+            if (from[g[x][i]] == -1 || Match(from[g[x][i]]))
+            {
+                from[g[x][i]] = x;
+                return true;
+            }
+        }
+    return false;
+}
+
+int Hungary()
+{
+    ans = 0;
+    memset(from, 255, sizeof from);
+    for (int i = 1; i <= n; i++)
+    {
+        memset(use, 0, sizeof use);
+        if (Match(i))
+            ans++;
+    }
+    return ans;
+}
+
+int main()
+{
+    int t, m, tem, x;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d%d", &n, &m);
+        for (int i = 1; i <= n; i++)
+            g[i].clear();
+        for (int i = 1; i <= n; i++)
+        {
+            scanf("%d", &tem);
+            for (int j = 0; j < tem; j++)
+            {
+                scanf("%d", &x);
+                g[i].push_back(x);
+            }
+        }
+        if (n == Hungary())
+            puts("YES");
+        else
+            puts("NO");
+    }
+    return 0;
+}
