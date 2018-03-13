ea3eb2a4fa4d4ddce3eb29ec18e54501d0273c01
Joe <joezhang@outlook.com>
2018-03-13 19:59:29 +0800

    temp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,75 +1,38 @@
-#include <stdio.h  >
-#include <string.h>
-#define N 20
-struct jie
+#include<iostream>
+#include<cstring>
+
+#define MAXN 1000
+using namespace std;
+struct MyMatrix
 {
-    char name[N];
-    int t;
-    int list[20];
-    int res;
+    int n, m;
+    int a[MAXN][MAXN];
+    void clear()
+    {
+        n = m = 0;
+        memset(a, 0, sizeof a);
+    }
+    MyMatrix operator*(const MyMatrix &x) const
+    {
+        MyMatrix tem;
+        tem.clear();
+        tem.n = n;
+        tem.m = m;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < x.m; j++)
+                for (int k = 0; k < m; k++)
+                    tem.a[i][j] = a[i][k] * x.a[k][j];
+        return tem;
+    }
 };
-jie a[2000];
+
+
+
 int main()
 {
-    jie b;
-    int n, m, g, count;
-    int sc[20], i, j, k, temp;
-    char tmp[N];
-    while (1)
-    {
-        count = 0;
-        scanf("%d", &n);
-        if (n == 0)
-            break;
-        scanf("%d%d", &m, &g);
-        for (i = 1; i <= m; i++)
-        {
-            scanf("%d", &sc[i]);
-        }
-        for (i = 1; i <= n; i++)
-        {
-            scanf("%s", b[i].name);
-            scanf("%d",&a[i].t);
-            for (j = 1; j <= a[i].t; j++)
-            {
-                scanf("%d", &a[i].list[j]);
-            }
-        }
-        for (i = 1; i <= n; i++)
-        {
-            a[i].res = 0;
-            for (j = 1; j <= a[i].t; j++)
-            {
-                a[i].res += sc[list[j]];
-            }
-            if (a[i].res >= g)
-                count++;
-        }
-        for (i = 1; i < n; i++)
-        {
-            k = i;
-            for (j = 1; j <= n; j++)
-            {
-                if (a[k].res > a[j].res)
-                    k = j;
-            }
-            if (k != i)
-            {
-                strcpy(tmp, a[k].name);
-                strcpy(a[k].name, a[i].name);
-                strcpy(a[i].name, tmp);
-                a[k].res = temp;
-                a[k].res = a[i].res;
-                a[i].res = temp;
-            }
-        }
-        printf("%d\n", count);
-        for (i = 1; i <= n; i++)
-        {
-            if (a[i].res >= g)
-            {
-                printf("%s %d\n", a[i].name, a[i].res);
-            }
-        }
-    }
+    MyMatrix a;
+    a.n=a.m = 10;
+    int n = 16;
+    cout << int(n&1) << endl;
+    return 0;
 }
