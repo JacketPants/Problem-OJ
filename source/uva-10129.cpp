1ef466398f689db0d431d3ac8f46c1f1b601a7da
Joe <joezhang@outlook.com>
2018-03-24 12:52:46 +0800

    Coding

--- source/uva-10129.cpp
+++ source/uva-10129.cpp
@@ -1,35 +1,46 @@
+#include <cstring>
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
-int t, n;
-vector<string> list;
-bool dfs(int cnt, char m)
+int g[30][30], n;
+int vis[30][30];
+int Euler(int u)
 {
-    if (cnt == n)
-        return true;
-    for (int i = 0; i < list.size(); i++)
-    {
-        
-    }
+    for (int v = 0; v < 26; v++)
+        if (vis[u][v] < g[u][v] && g[u][v])
+        {
+            vis[u][v]++;
+            return Euler(v) + 1;
+        }
+    return 0;
 }
+
 int main()
 {
+    int t;
+    bool isEuler = false;
+    string s;
     cin >> t;
-    string tem;
     while (t--)
     {
-        list.clear();
+        isEuler = false;
+        memset(g, 0, sizeof g);
         cin >> n;
         for (int i = 0; i < n; i++)
         {
-            cin >> tem;
-            list.push_back(tem);
+            cin >> s;
+            g[*(s.begin()) - 'a'][*(s.end() - 1) - 'a']++;
         }
-        if (dfs(0, 0))
-            puts("Ordering is possible.");
+        for (int i = 0; i < 26 && !isEuler; i++)
+        {
+            memset(vis, 0, sizeof vis);
+            // cout << Euler(i) << endl;
+            isEuler = (Euler(i) == n);
+        }
+        if (isEuler)
+            cout << "Ordering is possible.\n";
         else
-            puts("The door cannot be opened.");
+            cout << "The door cannot be opened.\n";
     }
     return 0;
 }
