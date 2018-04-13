d8255a19e96b11fdde74e94ada81c14f25d1719f
Joe <joezhang@outlook.com>
2018-04-13 20:10:59 +0800

    test code

--- test.cpp
+++ test.cpp
@@ -1,8 +1,29 @@
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
+#include <time.h>
 using namespace std;
 int main()
 {
-    cout << fmod(7.5, 2);
+    char ch;
+    FILE *fp = fopen("testtxt", "w");
+    srand((unsigned int)time(NULL));
+    for (int i = 1; i <= 1024; i++)
+    {
+        if (!(i % 16))
+            fputc('\n', fp);
+        else
+        {
+            ch = rand() % 62;
+            if (ch < 10)
+                ch += '0';
+            else if (ch < 36)
+                ch += 'a' - 10;
+            else
+                ch += 'A' - 36;
+            fputc(ch, fp);
+        }
+    }
+    fclose(fp);
     return 0;
 }
