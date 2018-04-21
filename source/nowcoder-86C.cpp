f7694b2e1e0b7f18e627d83f148e0d07dab6664e
Joe <joezhang@outlook.com>
2018-04-21 21:30:43 +0800

    AC

--- source/nowcoder-86C.cpp
+++ source/nowcoder-86C.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 int main()
 {
-    string list[] = {algorithm, bitset, cctype, cerrno, clocale, cmath, complex, cstdio, cstdlib, cstring, ctime, deque, exception, fstream, functional, limits, list, map, iomanip, ios, iosfwd, iostream, istream, ostream, queue, set, sstream, stack, stdexcept, streambuf, string, utility, vector, cwchar, cwctype};
+    string s,list[] = {"algorithm", "bitset", "cctype", "cerrno", "clocale", "cmath", "complex", "cstdio", "cstdlib", "cstring", "ctime", "deque", "exception", "fstream", "functional", "limits", "list", "map", "iomanip", "ios", "iosfwd", "iostream", "istream", "ostream", "queue", "set", "sstream", "stack", "stdexcept", "streambuf", "string", "utility", "vector", "cwchar", "cwctype"};
+    int t;
+    bool isQian;
+    cin >> t;
+    while(t--)
+    {
+        isQian = false;
+        cin >> s;
+        for (int i = 0; i < 35;i++)
+            if(list[i]==s)
+                isQian = true;
+        if(isQian)
+            cout << "Qian\n";
+        else
+        {
+            cout << "Kun\n";
+        }
+    }
     return 0;
 }
