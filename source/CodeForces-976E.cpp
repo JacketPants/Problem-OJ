d1b18d0693c5a87201438f50d478b2e883b9b5a1
Joe <joezhang@outlook.com>
2018-05-06 21:05:28 +0800

    Coding

--- source/CodeForces-976E.cpp
+++ source/CodeForces-976E.cpp
@@ -1,83 +1,86 @@
-// #include <algorithm>
-// #include <cmath>
-// #include <iostream>
-
-// using namespace std;
-// pair<long long, long long> list[200000];
-// int n, a, b;
-// bool cmp(pair<long long, long long> x, pair<long long, long long> y)
-// {
-//     if(x.first - x.second == y.first - y.second)
-//         return
-//     return x.first - x.second > y.first - y.second;
-// }
-// bool cmpb(pair<long long, long long> x, pair<long long, long long> y)
-// {
-//     if (x.first * (1LL << a) - x.second == y.first * (1LL << a) - y.second)
-//         return x.first - x.second > y.first - y.second;
-//     return x.first * (1LL << a) - x.second < y.first * (1LL << a) - y.second;
-// }
-// int main()
-// {
-//     long long ans = 0;
-//     int tem;
-//     scanf("%d%d%d", &n, &a, &b);
-//     for (int i = 0; i < n; i++)
-//         scanf("%I64d%I64d", &list[i].first, &list[i].second);
-//     tem = max_element(list, list + n, cmpb) - list;
-//     list[tem].first *= (1LL << a);
-//     sort(list, list + n, cmp);
-//     for (int i = 0; i < b; i++)
-//         ans += max(list[i].first, list[i].second);
-//     for (int i = b; i < n; i++)
-//         ans += list[i].second;
-//     cout << ans << endl;
-//     return 0;
-// }
 #include <algorithm>
-#include <cctype>
-#include <cstdio>
-#include <cstring>
+#include <cmath>
 #include <iostream>
-#define LL long long
+
 using namespace std;
-int n, A, B, t;
-LL Ans, Sum, Num;
-struct xcw
+pair<long long, long long> list[200005];
+int n, a, b;
+long long m;
+bool cmp(pair<long long, long long> x, pair<long long, long long> y)
 {
-    LL hp, dam;
-    bool operator<(const xcw b) const { return (hp - dam) > (b.hp - b.dam); }
-} a[200005];
-LL read()
+    return x.first - x.second > y.first - y.second;
+}
+bool cmpb(pair<long long, long long> x, pair<long long, long long> y)
 {
-    LL ret = 0;
-    char ch = getchar();
-    bool f = 1;
-    for (; !isdigit(ch); ch = getchar())
-        f ^= !(ch ^ '-');
-    for (; isdigit(ch); ch = getchar())
-        ret = ret * 10 + ch - '0';
-    return ret;
+    // if (x.first * m - x.second == y.first * m - y.second)
+    //     return x.first - x.second > y.first - y.second;
+    return (x.first * m - x.second) - (x.first - x.second) < (y.first * m - y.second) - (x.first - x.second);
 }
 int main()
 {
-    //  freopen("E.in","r",stdin);
-    //  freopen("E.out","w",stdout);
-    n = read(), A = read(), B = read();
-    for (int i = 1; i <= n; i++)
-        a[i] = (xcw){read(), read()}, Num += a[i].dam;
-    if (!B)
+    long long ans;
+    int tem;
+    while (~scanf("%d%d%d", &n, &a, &b))
     {
-        cout << Num << endl;
-        return 0;
+        ans = 0;
+        for (int i = 0; i < n; i++)
+            scanf("%I64d%I64d", &list[i].first, &list[i].second);
+        m = 1LL << a;
+        sort(list, list + n, cmp);
+        tem = max_element(list, list + n, cmpb) - list;
+        list[tem].first *= m;
+        for (int i = 0; i < b; i++)
+            ans += max(list[i].first, list[i].second);
+        for (int i = b; i < n; i++)
+            ans += list[i].second;
+        cout << ans << endl;
     }
-    sort(a + 1, a + 1 + n);
-    for (int i = 1; i <= B; i++)
-        Sum += max(0LL, a[i].hp - a[i].dam);
-    for (int i = 1; i <= B; i++)
-        Ans = max(Ans, Sum - max(0LL, a[i].hp - a[i].dam) - a[i].dam + (a[i].hp << A));
-    for (int i = B + 1; i <= n; i++)
-        Ans = max(Ans, Sum - max(0LL, a[B].hp - a[B].dam) - a[i].dam + (a[i].hp << A));
-    cout << Ans + Num << endl;
     return 0;
 }
+// #include <algorithm>
+// #include <cctype>
+// #include <cstdio>
+// #include <cstring>
+// #include <iostream>
+// #define LL long long
+// using namespace std;
+// int n, A, B, t;
+// LL Ans, Sum, Num;
+// struct xcw
+// {
+//     LL hp, dam;
+//     bool operator<(const xcw b) const { return (hp - dam) > (b.hp - b.dam); }
+// } a[200005];
+// LL read()
+// {
+//     LL ret = 0;
+//     char ch = getchar();
+//     bool f = 1;
+//     for (; !isdigit(ch); ch = getchar())
+//         f ^= !(ch ^ '-');
+//     for (; isdigit(ch); ch = getchar())
+//         ret = ret * 10 + ch - '0';
+//     return ret;
+// }
+// int main()
+// {
+//     //  freopen("E.in","r",stdin);
+//     //  freopen("E.out","w",stdout);
+//     n = read(), A = read(), B = read();
+//     for (int i = 1; i <= n; i++)
+//         a[i] = (xcw){read(), read()}, Num += a[i].dam;
+//     if (!B)
+//     {
+//         cout << Num << endl;
+//         return 0;
+//     }
+//     sort(a + 1, a + 1 + n);
+//     for (int i = 1; i <= B; i++)
+//         Sum += max(0LL, a[i].hp - a[i].dam);
+//     for (int i = 1; i <= B; i++)
+//         Ans = max(Ans, Sum - max(0LL, a[i].hp - a[i].dam) - a[i].dam + (a[i].hp << A));
+//     for (int i = B + 1; i <= n; i++)
+//         Ans = max(Ans, Sum - max(0LL, a[B].hp - a[B].dam) - a[i].dam + (a[i].hp << A));
+//     cout << Ans + Num << endl;
+//     return 0;
+// }
