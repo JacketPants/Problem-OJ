f51e558ea1a652bad5e9ddfa01a92aeccba3c362
Joe <joezhang@outlook.com>
2018-05-13 18:13:58 +0800

    coding

---
+++ source/CodeForces-978D.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+int list[100001];
+int s[100001];
+int main()
+{
+    int n, m;
+    long long sum = 0;
+    cin >> n;
+    m = n - 1;
+    for (int i = 0; i < n; i++)
+        cin >> list[i];
+    for (int i = 0; i < m; i++)
+    {
+        s[i] = s[i + 1] - s[i];
+        sum += s[i];
+    }
+    if (!(sum % m == 0 || (sum + 1) % m == 0 || (sum - 1) % m == 0))
+        puts("-1");
+
+    else
+    {
+
+    }
+    return 0;
+}
