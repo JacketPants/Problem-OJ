b459df127e6b4dc8c09109ac6d7435b5b8e9b33a
Joe <joezhang@outlook.com>
2018-05-24 21:34:34 +0800

    AC

--- source/hdu-5705.cpp
+++ source/hdu-5705.cpp
@@ -9,10 +9,13 @@ int main()
     while (~scanf("%d:%d:%d", &hh, &mm, &ss))
     {
         scanf("%d", &a);
+        // 转化为秒数
         s = ss + mm * 60 + hh * 3600;
+        // 乘120以便于整数处理
         a *= 120;
         do
             s++;
+        // 每过1秒，时针走1°，分针走12°
         while (abs(abs((s * 12) % MOD - s % MOD) - a) > 10);
         printf("Case #%d: %0.2d:%0.2d:%0.2d\n", t++, (s / 3600) % 12, (s / 60) % 60, s % 60);
     }
