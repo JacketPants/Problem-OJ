82efa6687b070a754f75b66eedd5e3d4e5a158e0
Joe <joezhang@outlook.com>
2018-05-29 14:37:03 +0800

    AC

---
+++ source/hdu-6294.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool list[1000000];
+int main()
+{
+    ios::sync_with_stdio(false);
+    int t, len;
+    string s;
+    cin >> t;
+    while (t--)
+    {
+        cin >> len >> s;
+        list[s.length() - 1] = true;
+        for (int i = len - 2; i >= 0; i--)
+        {
+            if (s[i] < s[i + 1])
+                list[i] = false;
+            else if (s[i] > s[i + 1])
+                list[i] = true;
+            else
+                list[i] = list[i + 1];
+        }
+        for (int i = 0; i < len - 1; i++)
+            if (!list[i])
+                cout << '<';
+            else
+                cout << '>';
+        cout << '\n';
+    }
+    return 0;
+}
