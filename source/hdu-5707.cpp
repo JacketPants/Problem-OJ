4cac3140c28cc7dba6592242d49a780bf1571fc6
Joe <joezhang@outlook.com>
2018-05-29 14:36:58 +0800

    Coding

--- source/hdu-5707.cpp
+++ source/hdu-5707.cpp
@@ -1,14 +1,29 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
-
 int main()
 {
-    string a,b,c;
-    while(cin>>a>>b>>c)
+    string a, b, c;
+    int j, k;
+    bool is;
+    while (cin >> a >> b >> c)
     {
-        
+        j = k = 0;
+        is = a.length() + b.length() == c.length();
+        for (int i = 0; is && i < c.length(); i++)
+            if (j < a.length() && a[j] == c[i])
+                j++;
+            else if (k < b.length() && b[k] == c[i])
+                k++;
+            else
+                is = false;
+        cout << (is ? "Yes\n" : "No\n");
     }
     return 0;
 }
+/*
+
+abcdef
+
+*/
