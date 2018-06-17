178ff44a6df688072ee2d72c6bf41a0a18715d6d
Joe <joezhang@outlook.com>
2018-06-17 11:15:19 +0800

    AC

--- source/hdu-6297.cpp
+++ source/hdu-6297.cpp
@@ -1,27 +1,31 @@
-#include <iostream>
-
-using namespace std;
-
+#include <stdio.h>
 int main()
 {
-    int t;
-    int id, prob;
-    char team[20], s[20];
+    int t, id, prob, c, i;
+    char team[20], q[20];
     scanf("%d", &t);
     while (t--)
     {
-        scanf("%d%s%d%s", &id, team, &prob, s);
-        printf("%3d|%-16s|%4d|", id, team, prob);
-        if (s[0] == 'R')
+        char s[20] = "[          ]";
+        scanf("%d%s%d%s", &id, team, &prob, q);
+        if (q[1] == 'u')
         {
-            scanf("%d", &id);
-            for (int i = 0; i < id; i++)
+            scanf("%d", &c);
+            for (i = 1; i <= c; i++)
                 s[i] = 'X';
-            s[id] = '\0';
-            printf("[%-10s]\n", s);
+        }
+        else if (q[1] == 'B')
+        {
+            s[5] = 'A';
+            s[6] = 'C';
+            s[7] = '*';
         }
         else
-            printf("[    %-6s]\n", (s[0] == 'F' ? "AC*" : s));
+        {
+            for (i = 0; q[i]; i++)
+                s[i + 5] = q[i];
+        }
+        printf("%3d|%-16s|%4d|%s\n", id, team, prob, s);
     }
     return 0;
 }
