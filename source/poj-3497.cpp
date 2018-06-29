46bab9ce2c9e7798cda6430917a3d926acf33361
Joe <joezhang@outlook.com>
2018-06-29 18:27:01 +0800

    Coding

---
+++ source/poj-3497.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    int t, n;
+    long long b;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d%lld", &n, &b);
+    }
+    return 0;
+}
