38d1ca00eff65920494d480af0f6aecfdfea3691
Joe Zhang <joezhang@outlook.com>
2018-07-16 22:20:17 +0800

    AC

---
+++ source/FZU-1021.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstring>
+
+int main()
+{
+	int n,a,b,v[100],ans;
+	while(~scanf("%d",&n)&&n)
+	{
+		ans=0;
+		memset(v,0,sizeof v);
+		for(int i=0;i<n;i++)
+		{
+			scanf("%d%d",&a,&b);
+			for(int j=b+1;j<100;j++)
+				ans+=v[j];
+			ans%=1000000;
+			v[b]++;
+		}
+		printf("%d\n",ans);
+	}
+	return 0;
+}
