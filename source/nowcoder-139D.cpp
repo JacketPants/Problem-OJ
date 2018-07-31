88dcf818daaaf1ab17a7910b98808f96eb785b88
Joe <joezhang@outlook.com>
2018-07-31 15:17:46 +0800

    AC

--- source/nowcoder-139D.cpp
+++ source/nowcoder-139D.cpp
@@ -1,8 +1,9 @@
 #include <algorithm>
+#include <cstring>
 #include <iostream>
+#include <numeric>
 #include <unordered_set>
 #include <vector>
-
 #define G vector<Edge>
 
 using namespace std;
@@ -12,16 +13,20 @@ struct Edge
     int a, b;
 };
 bool tar[8][8];
+int id[8][8];
 int phi[8];
-
-unordered_set<G> ans;
+unordered_set<int> ans;
 int main()
 {
-    int n, m1, m2;
+    int n = 0, m1, m2, h;
     bool ok;
+    for (int i = 0; i < 8; i++)
+        for (int j = i + 1; j < 8; j++)
+            id[i][j] = id[j][i] = n++;
     while (~scanf("%d%d%d", &n, &m1, &m2))
     {
         G g1(m1), g2(m2), tem(m1);
+        ans.clear();
         memset(tar, 0, sizeof tar);
         for (int i = 0; i < m1; i++)
         {
@@ -35,21 +40,22 @@ int main()
             g2[i].a--;
             g2[i].b--;
         }
-        for (int i = 0; i < g2.size(); i++)
-        {
+        for (int i = 0; i < m2; i++)
             tar[g2[i].a][g2[i].b] = tar[g2[i].b][g2[i].a] = true;
-        }
-        itoa(phi, phi + n, 0);
+        iota(phi, phi + n, 0);
         do
         {
+            h = 0;
             ok = true;
             for (int i = 0; i < m1; i++)
             {
                 tem[i].a = phi[g1[i].a];
                 tem[i].b = phi[g1[i].b];
+                h |= (1 << id[phi[g1[i].a]][phi[g1[i].b]]);
                 ok &= tar[tem[i].a][tem[i].b];
             }
-            ansa += ok;
+            if (ok)
+                ans.insert(h);
         } while (next_permutation(phi, phi + n));
         printf("%d\n", ans.size());
     }
