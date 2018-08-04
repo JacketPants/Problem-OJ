abf7b9a3d4f6f15e491dd89b58cea531d5914458
Joe <joezhang@outlook.com>
2018-08-04 19:45:15 +0800

    TEST

--- test.cpp
+++ test.cpp
@@ -1,115 +1,98 @@
-#include<bits/stdc++.h>  
-using namespace std;  
-const int N=2e5+5;  
-ll p,a[N],sum[N],s[N];  
-int n;  
-int lowbit(int i)  
-{  
-    return i&(-i);  
-}  
-void update(int i)  
-{  
-    while(i<=n)  
-    {  
-        sum[i]+=1;  
-        i+=lowbit(i);  
-    }  
-}  
-ll get_sum(ll i)  
-{  
-    ll ans=0;  
-    while(i)  
-    {  
-        ans+=sum[i];  
-        i-=lowbit(i);  
-    }  
-    return ans;  
-}  
-int main()  
-{  
-    while(~scanf("%d",&n))  
-    {  
-        memset(sum,0,sizeof(sum));  
-        memset(s,0,sizeof(s));  
-        ll ans=0,Ans=0;  
-        for(int i=1; i<=n; i++)  
-        {  
-            scanf("%lld",&a[i]);  
-            s[i]=a[i];  
-        }  
-        //离散化  
-        sort(s,s+n);  
-        int k=unique(s,s+n)-s;  
-        for(int i=1; i<=n; i++) a[i]=(lower_bound(s,s+k,a[i])-s)+1;//保证下标从1开始  
-  
-        for(int i=1; i<=n; i++)  
-        {  
-            ll tmp=get_sum(a[i]);  
-            update(a[i]);  
-            ans+=tmp;//i前面小于等于a[i]的数个数  
-            Ans+=i-1-tmp;//i前面小于等于a[i]的数个数，逆序数  
-//        printf("i==%d a[i]==%lld tmp==%lld  ans==%lld\n",i,a[i],tmp,i-tmp);  
-        }  
-        printf("%lld\n",ans);//小于等于当前元素的个数总和  
-        printf("%lld\n",Ans);//求逆序数  
-    }  
-    return 0;  
-}
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <vector>
 
-#include<cstdio>
-#include<cstring>
-#include<iostream>
-#include<algorithm>
-#include<vector>
 using namespace std;
 
-const int MAXN = 500010;
-
-int n;
-int tmpNum[MAXN] , num[MAXN];
-int treeNum[MAXN];
-
-int lowbit(int x){
-    return x&(-x);
+struct Edge
+{
+    int to, next;
+    int index;
+    int dir;
+    bool flag;
+} edge[220];
+int head[10], tot;
+void init()
+{
+    memset(head, -1, sizeof(head));
+    tot = 0;
 }
-
-int getSum(int x){
-    int sum = 0;
-    while(x){
-        sum += treeNum[x];
-        x -= lowbit(x);
-    }
-    return sum;
+void addedge(int u, int v, int index)
+{
+    edge[tot].to = v;
+    edge[tot].next = head[u];
+    edge[tot].index = index;
+    edge[tot].dir = 0;
+    edge[tot].flag = false;
+    head[u] = tot++;
+    edge[tot].to = u;
+    edge[tot].next = head[v];
+    edge[tot].index = index;
+    edge[tot].dir = 1;
+    edge[tot].flag = false;
+    head[v] = tot++;
 }
-
-void add(int x , int val){
-    while(x < MAXN){
-         treeNum[x] += val;
-         x += lowbit(x);
-    }
+int du[10];
+vector<int> ans;
+void dfs(int u)
+{
+    for (int i = head[u]; i != -1; i = edge[i].next)
+        if (!edge[i].flag)
+        {
+            edge[i].flag = true;
+            edge[i ^ 1].flag = true;
+            dfs(edge[i].to);
+            ans.push_back(i);
+        }
 }
-
-long long solve(){
-    long long ans = 0;
-    memcpy(tmpNum , num , sizeof(num));
-    memset(treeNum , 0 , sizeof(treeNum));
-    sort(num+1 , num+1+n);
-    int len = unique(num+1 , num+1+n)-(num+1);
-    for(int i = 1 ; i <= n ; i++){
-        int id = lower_bound(num+1 , num+1+n,tmpNum[i])-num;
-        ans += i-getSum(id)-1;
-        add(id , 1);
-    }
-    return ans;
-}
-
 int main()
 {
-    int x, y;
-    while(scanf("%d%d%d" , &n,&x,&y)!=EOF){
-         for(int i = 1 ; i <= n ; i++)
-             scanf("%d" , &num[i]);
-         printf("%lld\n" , solve()*min(x,y));
+    int n;
+    while (scanf("%d", &n) == 1)
+    {
+        init();
+        int u, v;
+        memset(du, 0, sizeof(du));
+        for (int i = 1; i <= n; i++)
+        {
+            scanf("%d%d", &u, &v);
+            addedge(u, v, i);
+            du[u]++;
+            du[v]++;
+        }
+        int s = -1;
+        int cnt = 0;
+        for (int i = 0; i <= 6; i++)
+        {
+            if (du[i] & 1)
+            {
+                cnt++;
+                s = i;
+            }
+            if (du[i] > 0 && s == -1)
+                s = i;
+        }
+        if (cnt != 0 && cnt != 2)
+        {
+            printf("No solution");
+            continue;
+        }
+        ans.clear();
+        dfs(s);
+        if (ans.size() != n)
+        {
+            puts("No solution");
+            continue;
+        }
+        for (int i = 0; i < ans.size(); i++)
+        {
+            printf("%d ", edge[ans[i]].index);
+            if (edge[ans[i]].dir == 0)
+                puts("-");
+            else
+                puts("+");
+        }
     }
     return 0;
 }
