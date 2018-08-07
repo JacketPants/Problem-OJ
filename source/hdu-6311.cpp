b4dca520eb4485e75478e4524fbc8cc3c1ed3dee
Joe <joezhang@outlook.com>
2018-08-07 18:03:22 +0800

    coding

--- source/hdu-6311.cpp
+++ source/hdu-6311.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #define MAXN 100010
+#define MAXM 500050
 using namespace std;
 
 struct Edge
@@ -11,13 +12,27 @@ struct Edge
     int index;
     int dir;
     bool flag;
-} edge[220];
-int head[10], tot;
-void init()
+};
+Edge edge[MAXM];
+int head[MAXN], fa[MAXN], du[MAXN], sum, tot;
+vector<int> ans[MAXN], sp[MAXN], tem;
+void init(int n)
 {
     memset(head, -1, sizeof head);
-    tot = 0;
+    memset(edge, 0, sizeof edge);
+    sum = tot = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        ans[i].clear();
+        sp[i].clear();
+        fa[i] = i;
+    }
 }
+int getfa(int u)
+{
+    return u == fa[u] ? u : fa[u] = getfa(fa[u]);
+}
+
 void addedge(int u, int v, int index)
 {
     edge[tot].to = v;
@@ -33,8 +48,6 @@ void addedge(int u, int v, int index)
     edge[tot].flag = false;
     head[v] = tot++;
 }
-int du[10];
-vector<int> ans;
 void dfs(int u)
 {
     for (int i = head[u]; i != -1; i = edge[i].next)
@@ -43,15 +56,18 @@ void dfs(int u)
             edge[i].flag = true;
             edge[i ^ 1].flag = true;
             dfs(edge[i].to);
-            ans.push_back(i);
+            ans[sum].push_back(i);
         }
 }
 int main()
 {
+    freopen("1003.in", "r", stdin);
+    freopen("out", "w", stdout);
+
     int n, m;
-    while (~scanf("%d%d", &n, &m))
+    while (cin >> n >> m)
     {
-        init();
+        init(n);
         int u, v;
         memset(du, 0, sizeof(du));
         for (int i = 1; i <= m; i++)
@@ -60,38 +76,71 @@ int main()
             addedge(u, v, i);
             du[u]++;
             du[v]++;
+            fa[getfa(u)] = getfa(v);
         }
-        int s = -1;
-        int cnt = 0;
-        for (int i = 0; i <= n; i++)
-        {
+
+        for (int i = 1; i <= n; i++)
             if (du[i] & 1)
+                sp[getfa(i)].push_back(i);
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (fa[i] == i)
             {
-                cnt++;
-                s = i;
+                if (!sp[i].size())
+                {
+                    dfs(i);
+                    if (ans[sum].size())
+                        sum++;
+                }
+                else
+                {
+                    for (int j = 0; j < sp[i].size(); j += 2)
+                        addedge(sp[i][j], sp[i][j + 1], 0);
+                    dfs(i);
+                    tem = ans[sum];
+                    ans[sum].clear();
+                    int p = tem.size() - 1;
+                    while (edge[tem[p--]].index)
+                        ;
+                    for (int j = p; j >= 0; j--)
+                    {
+                        if (edge[tem[j]].index)
+                            ans[sum].push_back(tem[j]);
+                        else
+                        {
+                            // if (ans[sum].size())
+                            sum++;
+                        }
+                    }
+                    for (int j = tem.size() - 1; j > p + 1; j--)
+                        ans[sum].push_back(tem[j]);
+                    // if (ans[sum].size())
+                    sum++;
+                }
             }
-            if (du[i] > 0 && s == -1)
-                s = i;
         }
-        if (cnt != 0 && cnt != 2)
+        int anssum = sum;
+        for (int i = 0; i < sum; i++)
+            if (!ans[i].size())
+                anssum--;
+
+        printf("%d\n", anssum);
+
+        for (int i = 0; i < sum; i++)
         {
-            printf("No solution\n");
-            continue;
-        }
-        ans.clear();
-        dfs(s);
-        if (ans.size() != n)
-        {
-            printf("No solution\n");
-            continue;
-        }
-        for (int i = 0; i < ans.size(); i++)
-        {
-            printf("%d ", edge[ans[i]].index);
-            if (edge[ans[i]].dir == 0)
-                printf("−\n");
-            else
-                printf("+\n");
+            if (ans[i].size())
+            {
+                printf("%d", ans[i].size());
+                for (int j = 0; j < ans[i].size(); j++)
+                {
+                    putchar(' ');
+                    if (edge[ans[i][j]].dir)
+                        putchar('-');
+                    printf("%d", edge[ans[i][j]].index);
+                }
+                putchar('\n');
+            }
         }
     }
     return 0;
