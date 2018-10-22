65ebdd450c066fa685b426f5e41b2da32b10eb01
Joe Zhang <joezhang@outlook.com>
2018-10-22 20:40:08 +0800

    AC

--- source/hdu-2671.cpp
+++ source/hdu-2671.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int t, ax, ay, bx, by, cx, cy;
-    double k, b;
+    double k, b, ans;
     cin >> t;
     while (t--)
     {
@@ -13,11 +13,15 @@ int main()
         b = cy - k * cx;
         if ((ax * k + b > ay && bx * k + b > by) || (ax * k + b < ay && bx * k + b < by))
         {
+            double x = ax - (2 * k * ((k * ax - ay + b) / (k * k + 1)));
+            double y = ay - (2 * (-1) * ((k * ax - ay + b) / (k * k + 1)));
+            ans = sqrt((x - bx) * (x - bx) + (y - by) * (y - by));
         }
         else
         {
-            
+            ans = sqrt((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
         }
+        printf("%.2lf\n", ans);
     }
     return 0;
 }
