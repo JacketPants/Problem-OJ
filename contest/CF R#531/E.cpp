85fb66ad075d947c5259b2dcbc9e191b5a27734b
Joe Zhang <joezhang@outlook.com>
2019-01-19 22:33:21 +0800

    AC

--- contest/CF R#531/E.cpp
+++ contest/CF R#531/E.cpp
@@ -1,40 +1,23 @@
-/*
-比赛时未写完，思路是：
-由题目可知推论，一个数字n，如果其第一次出现位置为a_i，最后一次出现位置为a_j，则b_i到b_j之间的元素必定一样 
-
-从左向右遍历a数组，设置变量last表示当前遇到的最后一次出现位置。
-对于每次遍历，进行到i元素时，如果i>last，则令cnt++，之后寻找i元素的最后一次更新位置，并更新last
-
-cnt为拐点数 
-答案为2^拐点数 
-时间复杂度为O(N^2) 
-*/
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <map>
+#define MOD 998244353
 using namespace std;
-int l[200005];
-bool u[200005]; 
+int a[200005];
+map<int, int> l;
 int main()
 {
-	memset(u,-1,sizeof u);
-	int n;
-	cin>>n;
-	for(int i=0;i<n;i++)
-		scanf("%d",l+i);
-	for(int i=0;i<n;i++)
+	int n, ans = 1, now;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		scanf("%d", a + i);
+	for (int i = 0; i < n; i++)
+		l[a[i]] = i;
+	now = l[a[0]];
+	for (int i = 1; i < n; i++)
 	{
-		if(u[i])
-		{
-			for(int j=n-1;j>i;j--)
-			{
-				if(l[i]==l[j])
-				{
-					for(int k=i;k<=j;k++)
-						u[i]=false;
-					break;
-				}
-			}
-		}
+		ans = i > now ? (2 * ans) % MOD : ans;
+		now = l[a[i]] > now ? l[a[i]] : now;
 	}
+	cout << ans << endl;
 	return 0;
 }
