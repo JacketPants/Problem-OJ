38dde0348b731f8cdc336d7be9c656f5961a8caa
Joe Zhang <joezhang@outlook.com>
2019-03-12 13:48:19 +0800

    Coding

---
+++ contest/2019牛客寒假算法基础集训营5/H.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int list[5005];
+int main()
+{
+	int n;
+	long long k;
+	scnaf("%d%lld",&n,&k);
+	for(int i=0;i<n;i++)
+		scanf("%d",list+i);
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+		{
+			
+		}
+	return 0;
+} 
